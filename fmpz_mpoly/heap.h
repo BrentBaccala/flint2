481489a6f32aec4ac39a3cdf1710d0f70338a0e7
Brent Baccala <cosine@freesoft.org>
2021-04-18 00:15:01 -0400

    while debugging mul_johnson, use fmpz_set instead of copy in heap algorithm

--- fmpz_mpoly/heap.h
+++ fmpz_mpoly/heap.h
@@ -16,9 +16,11 @@ typedef struct fmpz_mpoly_heap_s
 /* #define HEAP_RIGHT(i) (2*(i) + 1) */
 /* #define HEAP_PARENT(i) ((i)/2) */
 
+/* This does evaluate h twice, but in our usage it doesn't matter */
+
 #define FMPZ_POLY_HEAP_ASSIGN(h, c1, c2) \
    do {                                \
-      fmpz_set(h.exp, c1);             \
+      fmpz_set((h).exp, c1);           \
       (h).next = (c2);                 \
    } while (0)
 
@@ -32,27 +34,35 @@ void * _fmpz_mpoly_heap_pop(fmpz_mpoly_heap_s * heap, slong * heap_len)
    i = 1;
    j = 2;
 
+   fmpz_init(exp);
+
    while (j < s)
    {
       if (!__fmpz_gt(heap[j].exp, heap[j + 1].exp))
          j++;
-      heap[i] = heap[j];
+      fmpz_set(heap[i].exp, heap[j].exp);
+      heap[i].next = heap[j].next;
       i = j;
       j = HEAP_LEFT(j);
    }
 
    /* insert last element into heap[i] */
-   *exp = *(heap[s].exp);
+   fmpz_set(exp, heap[s].exp);
    j = HEAP_PARENT(i);
 
    while (i > 1 && __fmpz_gt(exp, heap[j].exp))
    {
-      heap[i] = heap[j];
+      fmpz_set(heap[i].exp, heap[j].exp);
+      heap[i].next = heap[j].next;
       i = j;
       j = HEAP_PARENT(j);
    }
 
-   heap[i] = heap[s];
+   fmpz_set(heap[i].exp, heap[s].exp);
+   heap[i].next = heap[s].next;
+
+   // only needed for optimization, to drop unneeded pointers
+   // fmpz_zero(heap[s].exp);
 
    return x; 
 }
@@ -102,11 +112,14 @@ int _fmpz_mpoly_heap_insert(fmpz_mpoly_heap_s * heap, fmpz_t exp, void * x,
 
    while (n > i)
    {
-      heap[n] = heap[HEAP_PARENT(n)];
+      fmpz_set(heap[n].exp, heap[HEAP_PARENT(n)].exp);
+      heap[n].next = heap[HEAP_PARENT(n)].next;
       n = HEAP_PARENT(n);
    }
 
-   FMPZ_POLY_HEAP_ASSIGN(heap[i], exp, x);
+   //FMPZ_POLY_HEAP_ASSIGN(heap[i], exp, x);
+   fmpz_set(heap[i].exp, exp);
+   heap[i].next = x;
 
    return 1;
 }
